f2775e7fc69286328020017ae2d68ae0e08a31e3
Ksp23 <69498915+Ksp23@users.noreply.github.com>
2022-09-01 14:47:08 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cmndfr2xrbbf.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "keesengpong-employee"
+customregion = "us-east-1"
